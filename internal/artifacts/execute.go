d2c1cca39091d69ca5b2557704dd0b901f134bb6
HemalaDev57 <hdevanan@cloudbees.com>
2025-05-02 14:05:29 +0530

    Test commit

--- internal/artifacts/execute.go
+++ internal/artifacts/execute.go
@@ -54,7 +54,7 @@ func setEnvVars(cfg *Config) error {
 	if cloudBeesApiUrl == "" {
 		return fmt.Errorf(CloudbeesApiUrl + " is not set in the environment")
 	}
-	cfg.CloudBeesApiUrl = "http://localhost:8087"
+	cfg.CloudBeesApiUrl = "https://localhost:8097"
 
 	// cloudBeesApiToken := os.Getenv(CloudbeesApiToken)
 	// if cloudBeesApiToken == "" {
@@ -184,7 +184,7 @@ func sendCloudEvent(cloudEvent cloudevents.Event, config *Config) error {
 	// req, _ := http.NewRequest(PostMethod, getCloudbeesFullUrl(config), bytes.NewBuffer(eventJSON))
 	fmt.Println(PrettyPrint(cloudEvent))
 	// For Local Testing
-	req, _ := http.NewRequest(PostMethod, "http://localhost:8087/v3/external-events", bytes.NewBuffer(eventJSON))
+	req, _ := http.NewRequest(PostMethod, "https://localhost:8097/v3/external-events", bytes.NewBuffer(eventJSON))
 
 	req.Header.Set(ContentTypeHeaderKey, ContentTypeCloudEventsJson)
 	req.Header.Set(AuthorizationHeaderKey, Bearer+oidcToken)
